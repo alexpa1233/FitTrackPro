176f43e04a26f44800d0262539805f6f6fd33a23
alexpa1233 <alexpa12333@gmail.com>
2025-02-22 11:12:47 +0100

    fix: :bug: Correct routines migration
    Resolve the problem had my migration routines code

--- database/migrations/2025_02_21_141947_create_routines_table.php
+++ database/migrations/2025_02_21_141947_create_routines_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('routines', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->default(0)->constrained('users')->nullOnDelete();
+            $table->foreignId('user_id')->constrained('users')->onDelete(null)->default(0);
             $table->string('name');
             $table->timestamps();
         });
