b3f1cfea7bfc0374a20d7309faebab2b1e201fed
alexpa1233 <alexpa12333@gmail.com>
2025-03-13 11:54:58 +0100

    feat:

--- resources/js/pagebackoffice/User/UserDetail.jsx
+++ resources/js/pagebackoffice/User/UserDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import Config from '../../Config';
 
